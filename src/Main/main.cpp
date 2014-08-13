767014acae9a0093a055f03ba8ce1b0227c76053
Biskin <boskin.erkocevic99@yahoo.com>
2014-08-13 00:31:26 -0600

    Added comments
    Added more comments to main.cpp

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -8,43 +8,51 @@ int main(int argc, char* argv[]) {
     Matrix test1, test2, result;
     
     printf("Addition/Subtraction test!\n");
-    test1 = Matrix(3, 3);
-    test2 = Matrix(3, 3);
     
+    // Create 2 3x3 matrices
+    test1 = Matrix(3, 3);
     test1 = (double*)(const double[]){1, 1, 1,
                                       2, 2, 2,
                                       3, 3, 3};
-             
+    
+    test2 = Matrix(3, 3);
     test2 = (double*)(const double[]){3, 3, 3,
                                       2, 2, 2,
                                       1, 1, 1};
     
+    // Print the results
     printf("test1:\n");
     test1.consolePrint(WIDTH, DECIMALS);
     
     printf("test2:\n");
     test2.consolePrint(WIDTH, DECIMALS);
     
+    // Store the sum of the matrices in result and print
     result = test1 + test2;
     printf("sum:\n");
     result.consolePrint(WIDTH, DECIMALS);
     
+    // Store the difference of the matrices in result and print
     result = test1 - test2;
     printf("difference:\n");
     result.consolePrint(WIDTH, DECIMALS);
     
     printf("\nMultiplication test!\n");
     
+    // Create a 1x3 matrix
     test1 = Matrix(1, 3);
     test1 = (double*)(const double[]){1, 4, 6};
     
+    // Create a 3x2 matrix
     test2 = Matrix(3, 2);
     test2 = (double*)(const double[]){2, 3,
                                       5, 8,
                                       7, 9};
     
-    result = test1 * test2; // Multiply the matrices
+    // Store the product of the matrices in result
+    result = test1 * test2;
     
+    // Print the results
     printf("test1:\n");
     test1.consolePrint(WIDTH, DECIMALS);
     
@@ -56,14 +64,19 @@ int main(int argc, char* argv[]) {
     
     printf("\nIdentity matrix test!\n");
     
+    // Create a 2x2 matrix
     test1 = Matrix(2, 2);
     test1 = (double*)(const double[]){1, 2,
                                       3, 4};
     
+    // Create a 2x2 identity matrix and store it in test2
     test2 = Matrix::identity(2);
     
+    /* Store the product of the two matrices in result,
+       should be a matrix equivalent to test1 */
     result = test1 * test2;
     
+    // Print the results
     printf("test1:\n");
     test1.consolePrint(WIDTH, DECIMALS);
     
